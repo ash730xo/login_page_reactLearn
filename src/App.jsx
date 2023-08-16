a77f0a4b7a031759c10865dc9868d7c3b0280d7f
ash730xo <ash730@gmail.com>
2023-08-16 12:59:28 -0400

    console log each time the button is pushed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import React from "react"
+import { useState } from "react"
 import './styles.css'
 
 function App() {
+
+  function handleChange(e) {
+    console.log(event.target.value)
+  }
 
   return (
     <>
@@ -9,11 +13,11 @@ function App() {
         <div className="login_Container">
           <div className="username_Container">
             <label className="username_label">Username:</label>
-            <input type="text" className="username_Input"/>
+            <input type="text" className="username_Input" onChange={handleChange}/>
           </div>
           <div className="password_Container">
             <label className="password_label">Password:</label>
-            <input type="password" className="password_Input"/>
+            <input type="password" className="password_Input" onChange={handleChange}/>
           </div>
           <button className="submit">Submit</button>
         </div>
