b90dcb8c660d168ef1c1aaaad9a8e88d2b694252
ash730xo <ash730@gmail.com>
2023-08-16 13:22:52 -0400

    useState user & pass, submit button w/log input

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,40 @@ import { useState } from "react"
 import './styles.css'
 
 function App() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
 
-  function handleChange(e) {
-    console.log(event.target.value)
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log({username})
+    console.log({password})
   }
 
   return (
     <>
+      {/* Whole container */}
       <form className= "login_Screen">
+
+        {/* user name & pass container */}
         <div className="login_Container">
+
+          {/* username container */}
           <div className="username_Container">
             <label className="username_label">Username:</label>
-            <input type="text" className="username_Input" onChange={handleChange}/>
+            <input type="text" className="username_Input" value={username} onChange={e => setUsername(e.target.value)}/>
           </div>
+
+          {/* password container */}
           <div className="password_Container">
             <label className="password_label">Password:</label>
-            <input type="password" className="password_Input" onChange={handleChange}/>
+            <input type="password" className="password_Input" value={password} onChange={e => setPassword(e.target.value)}/>
           </div>
-          <button className="submit">Submit</button>
+
+          <button className="submit" type="submit" onClick={handleSubmit} >Submit</button>
         </div>
+        {/* submit button in login container */}
       </form>
+      
     </>
   )
 }
